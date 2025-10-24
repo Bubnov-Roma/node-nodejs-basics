e3a106b70fd68bc39f151e19f78e0b5911b49a27
Роман Бубнов <kilkun@icloud.com>
2025-10-25 00:24:56 +0400

    feat: implement streams/read.js task

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,3 +1,22 @@
+import { fileURLToPath } from 'url'
+import { join, dirname } from 'path'
+import { createReadStream } from 'fs'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 export const read = async () => {
-    // Write your code here 
+    const filePath = join(__dirname, './files/fileToRead.txt');
+    return new Promise((resolve, reject) => {
+        const readableStream = createReadStream(filePath, { encoding: 'utf-8' });
+        readableStream.pipe(process.stdout);
+        readableStream.on('end', () => {
+            process.stdout.write('\n');
+            resolve();
+        })
+        readableStream.on('error', (error) => {
+            reject(new Error('FS operation failed'))
+        })
+    })
 };
+
+read();
