7060efda33a0d1b97fde3573822673753b2e5e95
Роман Бубнов <kilkun@icloud.com>
2025-10-24 21:15:33 +0400

    feat: implement fs/delete.js task

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,3 +1,21 @@
+import { unlink, stat } from 'fs/promises';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 export const remove = async () => {
-    // Write your code here 
+    const filePath = join(__dirname, './files/fileToRemove.txt');
+    try {
+        const fileStats = await stat(filePath);
+        if (!fileStats.isFile()) {
+            throw new Error('FS operation failed');
+        }
+        await unlink(filePath);
+    } catch (error) {
+        throw new Error('FS operation failed');
+    }
 };
+
+remove();
