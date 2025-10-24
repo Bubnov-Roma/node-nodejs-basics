6e4359fe4e31d76288a302d77c27a5c2d51cee49
Роман Бубнов <kilkun@icloud.com>
2025-10-25 00:43:26 +0400

    feat: implement streams/write.js task

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,3 +1,25 @@
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+import { createWriteStream } from 'fs';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename)
+
 export const write = async () => {
-    // Write your code here 
+    const filePath = join(__dirname, './files/fileToWrite.txt');
+    return new Promise((resolve, reject) => {
+        const writableStream = createWriteStream(filePath, { encoding: 'utf-8' });
+        process.stdin.pipe(writableStream);
+        writableStream.on('finish', () => {
+            resolve();
+        });
+        writableStream.on('error', () => {
+            reject(new Error('FS operation failed'));
+        });
+        process.stdin.on('error', () => {
+            reject(new Error('FS operation failed'));
+        });
+    });
 };
+
+write();
