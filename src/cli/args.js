49357a3f5432068f2488aa36f487f5b68266d2a4
Роман Бубнов <kilkun@icloud.com>
2025-10-24 22:40:09 +0400

    feat: implement cli/args.js task

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,13 @@
 export const parseArgs = () => {
-    // Write your code here 
+    const args = process.argv.slice(2)
+    const result = args.reduce((acc, arg, index, array) => {
+        if (arg.startsWith('--') && array[index + 1]) {
+            const propNme = arg.slice(2)
+            const value = array[index + 1]
+            acc.push(`${propNme} is ${value}`)
+        }
+        return acc
+    }, [])
+    if (result.length > 0) console.log(result.join(', '))
 };
+parseArgs()
