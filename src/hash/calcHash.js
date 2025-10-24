58ed7476f199fd50cf439b9ad04119c78e75ced7
Роман Бубнов <kilkun@icloud.com>
2025-10-24 23:54:48 +0400

    feat: implement hash/calcHash.js task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,28 @@
+import { createReadStream } from 'fs';
+import { createHash } from 'crypto';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 export const calculateHash = async () => {
-    // Write your code here 
+    const filePath = join(__dirname, './files/fileToCalculateHashFor.txt');
+    return new Promise((resolve, reject) => {
+        const hash = createHash('sha256');
+        const stream = createReadStream(filePath);
+        stream.on('data', (chunk) => {
+            hash.update(chunk);
+        });
+        stream.on('end', () => {
+            const hexHash = hash.digest('hex');
+            console.log(hexHash);
+            resolve();
+        })
+        stream.on('error', (error) => {
+            reject(new Error('FS operation failed'))
+        });
+    });
 };
+
+calculateHash();
