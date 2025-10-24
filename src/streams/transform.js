4445067178648235e03d6aa8e3741f2d3dc59c86
Роман Бубнов <kilkun@icloud.com>
2025-10-25 00:52:53 +0400

    feat: implement streams/transform.js task

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,3 +1,23 @@
+import { Transform } from 'stream';
+import { pipeline } from 'stream/promises'
+
 export const transform = async () => {
-    // Write your code here 
+    const reverseTransform = new Transform({
+        transform(chunk, _encoding, callback) {
+            const reversedText = chunk.toString().split('').reverse().join('');
+            this.push(reversedText);
+            callback();
+        }
+    });
+    try {
+        await pipeline(
+            process.stdin,
+            reverseTransform,
+            process.stdout
+        );
+    } catch {
+        throw new Error('Stream operation failed');
+    }
 };
+
+transform();
