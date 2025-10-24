7ed05527838bfb6751420ba0637e76ed18101806
Роман Бубнов <kilkun@icloud.com>
2025-10-24 22:16:33 +0400

    feat: implement cli/env.js task

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,3 +1,16 @@
 export const parseEnv = () => {
-    // Write your code here 
+    const envVariables = process.env
+    const rssVariables = Object.entries(envVariables)
+        .filter(([key]) => key.startsWith('RSS_'))
+        .map(([key, value]) => `${key}=${value}`);
+    if (rssVariables.length > 0) {
+        console.log(rssVariables.join('; '))
+    }
 };
+
+process.env.RSS_name1 = 'value1';
+process.env.RSS_name2 = 'value2';
+process.env.RSS_name3 = 'value3';
+process.env.OTHER_VAR = 'should_not_appear';
+
+parseEnv();
