09b02537eb4a933f9191d7cea1acd34197e80da7
Роман Бубнов <kilkun@icloud.com>
2025-10-25 15:29:05 +0400

    feat: implement zip/compress.js task

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,3 +1,24 @@
+import { fileURLToPath } from 'url'
+import { dirname, join } from 'path';
+import { createReadStream, createWriteStream } from 'fs';
+import { createGzip } from 'zlib';
+import { pipeline } from 'stream/promises';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 export const compress = async () => {
-    // Write your code here 
+    const sourcePath = join(__dirname, './files/fileToCompress.txt');
+    const targetPath = join(__dirname, './files/archive.gz');
+
+    try {
+        const readableStream = createReadStream(sourcePath);
+        const writableStream = createWriteStream(targetPath);
+        const gzip = createGzip();
+        await pipeline(readableStream, gzip, writableStream);
+    } catch {
+        throw new Error('FS operation failed');
+    }
 };
+
+compress();
